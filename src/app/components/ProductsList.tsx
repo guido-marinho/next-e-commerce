4825380ad94d8e7fafba5fd8904e419675aef73c
guilhermegattimarinho <ggattimarinho@gmail.com>
2023-10-22 15:53:48 -0300

    ✨ feat: adiciona elemento de botão ao produtos

--- src/app/components/ProductsList.tsx
+++ src/app/components/ProductsList.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import { Product } from "../../Types/Product";
+import AddCartBtn from "./AddCartBtn";
 
 export default async function ProductsList() {
   const products = await getProducts();
@@ -7,18 +8,20 @@ export default async function ProductsList() {
     <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
       {
         products.map((product: Product) => (
-          <li key={product.id} className="bg-white rounded-lg shadow-lg">
-            <a href="#" className="block relative h-48 rounded overflow-hidden">
+          <li key={product.id} className="bg-white rounded-lg shadow-lg flex flex-col">
+            <div className="block relative h-48 rounded overflow-hidden">
               <img
                 src={product.image}
                 alt={product.title} className="object-cover object-center w-full h-full block"
               />
-            </a>
+            </div>
             <div className="p-4">
               <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">{product.category}</h3>
               <h2 className="text-gray-900 title-font text-lg font-medium">{product.title}</h2>
               <p className="mt-1">${product.price}</p>
             </div>
+
+            <AddCartBtn/>
           </li>
         ))
       }

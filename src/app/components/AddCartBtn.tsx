ef9bf68f8a436c26f1f2cd1bdeed1e2644f1df5f
guilhermegattimarinho <ggattimarinho@gmail.com>
2023-10-22 17:03:46 -0300

    🔥 fix: deleta antigo addcartbtn

--- src/app/components/AddCartBtn.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function AddCartBtn() {
-  return (
-    <button className="rounded-md bg-teal-600 text-white px-3 py-2 text-center">
-      Adicionar ao carrinho
-    </button>
-  )
-}
